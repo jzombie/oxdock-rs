b7a3ba9e13a0e91097ab8b43a3f18a49049680ad
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-12-19 15:28:53 -0600

    Debug rust-analyzer detection

--- oxdock-buildtime-macros/build.rs
+++ oxdock-buildtime-macros/build.rs
@@ -1,5 +1,7 @@
 fn main() {
     println!("cargo:rustc-check-cfg=cfg(rust_analyzer)");
+    println!("cargo:rerun-if-env-changed=RUSTC_WRAPPER");
+    println!("cargo:rerun-if-env-changed=RUSTC_WORKSPACE_WRAPPER");
 
     if std::env::var("RUSTC_WRAPPER").map_or(false, |v| v.contains("rust-analyzer"))
         || std::env::var("RUSTC_WORKSPACE_WRAPPER").map_or(false, |v| v.contains("rust-analyzer"))
